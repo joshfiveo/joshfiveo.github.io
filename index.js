56c7d92e333da0ea9bdf1a5c3e508d57784bf323
joshfiveo <72544859+joshfiveo@users.noreply.github.com>
2020-10-08 20:26:51 +0200

    update

---
+++ index.js
@@ -0,0 +1,18 @@
+let invokeButton = document.getElementsByClassName("invokeButton");
+let passageButton = document.getElementsByClassName("passageButton");
+
+function getRandomIntInclusive() {
+    return Math.floor(Math.random() * 100);
+}
+
+function invokePoem(){
+    alert("Poem " + getRandomIntInclusive() + " has been invoked!")
+}
+
+function invokePassage(){
+    alert("Passage " + getRandomIntInclusive() + " was summoned forth.")
+}
+
+invokeButton.onclick = invokePoem;
+passageButton.onclick = invokePassage;
+
