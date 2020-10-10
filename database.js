e6d66b11ef3ce00b3e1861312656b04ec0c55e99
joshfiveo <72544859+joshfiveo@users.noreply.github.com>
2020-10-10 17:55:13 +0200

    tlf update

---
+++ database.js
@@ -0,0 +1,7 @@
+let poem1 = document.getElementsByClassName("poem1");
+
+let showPoem = function(){
+    document.getElementsByClassName("monkepic").style.display = "block";
+}
+
+poem1.onclick = showPoem;
