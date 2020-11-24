6c442e07e920b0e32338abe4e3723fdd7a8cbfd3
joshfiveo <72544859+joshfiveo@users.noreply.github.com>
2020-11-24 08:30:33 +0100

    dsandsan

---
+++ database.js
@@ -0,0 +1,98 @@
+// DATABASE STUFF
+
+// Making buttons
+let makeButtons = function(){
+    for (i = 1; i < 43; i++){
+        let button = document.createElement('a');
+        let text = document.createElement('p')
+        text.innerHTML = `Poem ${i}`;
+        button.href = "#";
+
+        button.appendChild(text);
+        document.getElementById('poemsContainer').appendChild(button);
+    }
+}
+
+window.onload = makeButtons;
+
+//Clicking poem button
+let srcNr;
+
+let poemButtons = document.getElementsByTagName('a');
+
+let showPoem = function(event){
+    //making img and setting src
+    let img = document.createElement('img');
+    let poemNr = event.target.innerHTML;
+    let poemNrRefined = poemNr.replace(/\D/g,'');
+    srcNr = parseInt(poemNrRefined);
+    img.id = "img";
+
+    img.src = `./mcdatabase/poem(${poemNrRefined}).jpg`;
+    img.style.height = "100%";
+
+    //appending img to div and making div visible
+    document.getElementById('poemImgDiv').appendChild(img);
+
+    document.getElementById('poemImgDiv').style.display = "block";
+
+    //close button
+    let escape = document.createElement('button');
+    escape.innerHTML = "Close";
+
+    //Nnext button
+    let next = document.createElement('button');
+    next.innerHTML = ">";
+    next.id = "next";
+
+    //Prev Button
+    let prev = document.createElement('button');
+    prev.innerHTML = "<";
+    prev.id = "prev";
+
+    document.body.appendChild(escape);
+    document.body.appendChild(next);
+    document.body.appendChild(prev);
+}
+
+window.addEventListener('load', function (){
+    for (i = 0; i < poemButtons.length; i++){
+        poemButtons[i].addEventListener('click', showPoem);
+    }
+});
+
+let hidePoem = function(event){
+    if (event.target.innerHTML === "Close") {
+        document.getElementById('poemImgDiv').style.display = "none";
+        document.getElementById('poemImgDiv').firstElementChild.remove();
+        document.body.lastElementChild.remove();
+        document.body.lastElementChild.remove();
+        document.body.lastElementChild.remove();
+    } else if (event.target.id === "next" && srcNr !== 42){
+        srcNr++;
+        document.getElementById('img').src = `./mcdatabase/poem(${srcNr}).jpg`;
+    } else if (event.target.id === "prev" && srcNr !== 1){
+        srcNr--;
+        document.getElementById('img').src = `./mcdatabase/poem(${srcNr}).jpg`;
+    }
+}
+
+document.addEventListener('click', hidePoem);
+document.addEventListener("keydown", function (event) {
+    const key = event.key;
+    if (key === "ArrowRight" && srcNr !== 42) {
+        srcNr++;
+        document.getElementById('img').src = `./mcdatabase/poem(${srcNr}).jpg`;
+    }
+    if (key === "ArrowLeft" && srcNr !== 1) {
+        srcNr--;
+        document.getElementById('img').src = `./mcdatabase/poem(${srcNr}).jpg`;
+    }
+    if (key === "Escape") {
+        document.getElementById('poemImgDiv').style.display = "none";
+        document.getElementById('poemImgDiv').firstElementChild.remove();
+        document.body.lastElementChild.remove();
+        document.body.lastElementChild.remove();
+        document.body.lastElementChild.remove();
+    }
+});
---
+++ script.js
@@ -0,0 +1,88 @@
+//INDEX TING
+
+//Random integer
+function randomIntPoems(){
+    return Math.floor(Math.random() * 42) + 1;
+}
+
+//get random poem
+let randomPoem = function(){
+    //making img and setting src
+    let img = document.createElement('img');
+    img.id = "img";
+
+    img.src = `./mcdatabase/poem(${randomIntPoems()}).jpg`;
+    img.style.height = "100%";
+
+    //appending img to div and making div visible
+    document.getElementById('poemImgDiv').appendChild(img);
+
+    document.getElementById('poemImgDiv').style.display = "block";
+
+    //close button
+    let escape = document.createElement('button');
+    escape.innerHTML = "Close";
+
+    //REROLL BUTTON
+    let reroll = document.createElement('button');
+    reroll.innerHTML = "reroll";
+    reroll.id = "reroll";
+
+    document.body.appendChild(escape);
+    document.body.appendChild(reroll);
+}
+
+let randomPoemButton = document.getElementById('randomPoem');
+
+randomPoemButton.addEventListener('click', randomPoem);
+
+//close
+
+let hidePoem = function(event){
+    if (event.target.innerHTML === "Close") {
+        document.getElementById('poemImgDiv').style.display = "none";
+        document.getElementById('poemImgDiv').firstElementChild.remove();
+        document.body.lastElementChild.remove();
+        document.body.lastElementChild.remove();
+    } else if (event.target.innerHTML === "reroll" && document.getElementById('poemImgDiv').firstElementChild.tagName === "IMG"){
+        document.getElementById('img').src = `./mcdatabase/poem(${randomIntPoems()}).jpg`;
+    } else if (event.target.innerHTML === "reroll" && document.getElementById('poemImgDiv').firstElementChild.tagName === "P"){
+        document.getElementById('poemImgDiv').firstElementChild.innerHTML = passagesArray[randomIntPassages()];
+    }
+}
+
+document.addEventListener('click', hidePoem);
+
+//get random line(s)
+let passagesArray = ["he was a fur venerate / an utter degenerate", "they gargled the jizzom / but read no john grisham", "limp dick doug dimmadomes get sliced like cabbage", "i have one rule Brr / i spend all souls on Strr / Big pecs = big checks and fat stacks", "to plow with his stick / until he was sick", "drip drop, slip slop, woop-de-scoop-de-meme'in / knick-knack paddy whack, lickety-split, hacks eat shit", "big dick monkey samba flatten bipedal bitches / [..] / Bonobo bimbo bitches blow benis", "Hillary's clit smells like sardines / flush her down the pipes / shit on the kikes / finish the show with combat wipes", "a man did scam a dyke; what sham! FUCK him!", '"copacetic cephalopod cunts" harkd Jim', "call me Ted Munky, the twisted Simian Serial Killer [..] Bombing the loo, like I'm Ted Kachimpsky", "Hey! Sergei: my name isphone please tell my mom", "unbearably loud I coomed on my couch / when I saw Jenna Haze, I was left in a daze / and died with my peener ablaze", "so I strangle my stiffy until it goes bendy / I'm dissappointing a wench named Wendy", "pop fat souls to increase your swole, hoebag", "a wreckening reckoning: bumhole banishment", "His name: Larry Snakes. His testicles bloom'd / Bloom'd they did, women swoon'd; love reborn and: / five-finger fist fucks, a tongue-punch'd fartbox", "seasoned with suffering, an inhuman McOffering", "there once was a cuck in a town", "I blew out the shitter / I don't know what hit her", "with whip in hand, he prepares for hate crime", "the untimely termination of fellatio gave the erstwhile guzzler leverage", "you're left cooming in your room in a jizz salto", "blow your rectal trumpets Gabriel", "to poke and prod with massive, pulsing rod", "eldritch effluviate engulfed England / Rich royal rightous racists repress'd Rembrandt / painting purple pompous painful ass", "'to check yourself before you wreck yourself is sound advice' hark'd Snakes and took a shit", "And met a lad nam'd Lee who was to be / a cocksman of the highest order", "Paul Bunyuns bunion for the rings of McOnion", "Angela Merkel nukes the Ukranians", "it rot, then smell'd, then he said: lets have fun / we'll have loads of fun with rape and pillage", "a kike he drew a knife to fight, and lurk", "the world is dark and black with death, it sucks / I hate these fucking cooming zoomer cucks", "'Laughing, Larry cut of his head, then said / You're dead, you fuck, I got the balls of gods' / while ye coomers were masturbating, I be decapitatin'", "My pleasure is my dick in a mouse trap", "'O, Ye Old God of Cod: Ye hath harness'd / The power of the ass' stated Larry", "finger much but hole? dat ass", "he took their wives, the chink, the horse the coon / He met zizek, he sniff'd a sea of coke", "Shaking his fist at the paleblood sky, he said: / 'with slugs, and his fav'rit weapon: pencils / from the depths of hell, I relieve at thee my guts", "follow the path of living[Larry] snakes", "all work and no play makes jack a mad breh"];
+
+function randomIntPassages(){
+    return Math.floor(Math.random() * passagesArray.length);
+}
+
+let randomLineButton = document.getElementById('randomLine');
+
+let randomLine = function(){
+    //making paragraph and adding text
+    let lines = document.createElement('p');
+    lines.innerHTML = passagesArray[randomIntPassages()];
+    lines.id = "lines";
+
+    //appending
+    document.getElementById('poemImgDiv').appendChild(lines);
+    document.getElementById('poemImgDiv').style.display = "block";
+
+    //close button
+    let escape = document.createElement('button');
+    escape.innerHTML = "Close";
+
+    //REROLL BUTTON
+    let reroll = document.createElement('button');
+    reroll.innerHTML = "reroll";
+    reroll.id = "reroll";
+
+    document.body.appendChild(escape);
+    document.body.appendChild(reroll);
+}
+
+randomLineButton.addEventListener('click', randomLine);
